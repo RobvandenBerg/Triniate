7085b535436c571f9e05d8b34b010288aeb52bbe
Rob van den Berg <R.S.vandenBerg-1@student.tudelft.nl>
2020-12-08 13:06:33 +0000

    Added the items into the MySQL schematic

--- mysql_schematic.sql
+++ mysql_schematic.sql
@@ -301,6 +301,8 @@ CREATE TABLE `villains` (
   KEY `room` (`room`)
 ) ENGINE=MyISAM AUTO_INCREMENT=24609 DEFAULT CHARSET=latin1;
 
+INSERT IGNORE INTO `items` VALUES (1,'Apple',5,'no',0),(2,'Bone',5,'no',0),(3,'Money',0,'no',0),(4,'NPC\'s stone',10,'yes',0),(5,'Potion small',20,'no',0),(6,'Potion big',50,'no',0),(7,'Blue potion small',20,'no',0),(8,'Blue potion big',50,'no',0),(9,'Potion green small',20,'no',0),(10,'Potion green big',50,'no',0),(11,'Potion purple small',20,'no',0),(12,'Potion purple big',50,'no',0),(13,'Potion yellow small',20,'no',0),(14,'Potion yellow big',50,'no',0),(15,'Meat',20,'no',0),(16,'Small log',10,'no',0),(17,'Wooden axe',30,'no',15),(18,'Stone axe',40,'no',25),(19,'Iron axe',50,'no',35),(20,'Golden axe',60,'no',45),(21,'Diamond axe',70,'no',55),(22,'Wooden pickaxe',30,'no',15),(23,'Stone pickaxe',40,'no',25),(24,'Iron pickaxe',50,'no',35),(25,'Golden pickaxe',60,'no',45),(26,'Diamond pickaxe',70,'no',55),(27,'Stick',2,'no',0),(28,'Eduard\'s ring',0,'yes',0),(29,'Skull',20,'no',0),(30,'Oak wood',10,'no',0),(31,'Spruce wood',10,'no',0),(32,'Birch wood',10,'no',0),(33,'Oak planks',5,'no',0),(34,'Spruce planks',6,'no',0),(35,'Birch planks',6,'no',0),(36,'Stone',10,'no',0),(37,'Iron',30,'no',0),(38,'Gold',45,'no',0),(39,'Diamond',60,'no',0);
+
 
 INSERT IGNORE into pages (id, type, in_room, sells, retailer) VALUES (1, 'shop', 40, '1,5,7,9,17,18,22,23,33', 'normal'), (2, 'bank', 34, '', 'normal');
 
