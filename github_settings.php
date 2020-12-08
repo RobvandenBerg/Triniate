365eeff1b0dbd466d1e8a20b05e35f17df3fae26
Rob van den Berg <R.S.vandenBerg-1@student.tudelft.nl>
2020-12-08 14:48:37 +0000

    Untracked db_info.php and github_settings.php

--- github_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mysql_credentials_file_location = __DIR__ . '/db_info.php';
-
-$triniate_homepage = 'http://triniate.com'; // Change this to the URL of your website
-$triniate_playpage = 'http://play.triniate.com'; // Change this to the URL where Triniate can be played
-
-?>
