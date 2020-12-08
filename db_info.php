6c4ce1a3b72a52833b187a278f0fa1feee4d2db6
Rob van den Berg <R.S.vandenBerg-1@student.tudelft.nl>
2020-12-08 14:51:05 +0000

    Re-added db_info.php and github_settings.php

---
+++ db_info.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Specify your MySQL credentials here to the Triniate database and make sure that in github_settings.php, $mysql_credentials_file_location = __DIR__ . '/db_info.php'
+*/
+
+$dbhost = 'localhost';
+$dbuser = '*USERNAME HERE*';
+$dbpass = '*PASSWORD HERE*';
+$db = '*DATABASE HERE*';
+
+
+?>
