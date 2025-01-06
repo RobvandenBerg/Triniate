5b7d3c47d0289c4886b7da28c17342d5e411dad8
BiZKiT <57576298+HotPizzaYT@users.noreply.github.com>
2025-01-06 11:42:53 -0600

    Remove error reporting (3)

--- functions/check_items.php
+++ functions/check_items.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 function check_has_item($player,$item,$mysql_active = true)
 {
 	global $dbhost,$dbuser,$dbpass,$db;
